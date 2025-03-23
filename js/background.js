15e93a8598f7ddfe281319d088d05d5cc372e470
hamsauno <hamsauno74@gmail.com>
2025-03-23 21:11:54 +0300

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
     document.addEventListener("DOMContentLoaded", function() {
-        document.body.style.backgroundImage = "url('background.jpg')"; 
+        document.body.style.backgroundImage = "url('img/background.jpg')"; 
         document.body.style.backgroundSize = "cover"; 
         document.body.style.backgroundPosition = "center"; 
         document.body.style.backgroundRepeat = "no-repeat";
