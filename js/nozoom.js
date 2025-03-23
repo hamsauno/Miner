8f20949233380d7463e8e1a8eccccfba1f7125ea
hamsauno <hamsauno74@gmail.com>
2025-03-23 21:36:57 +0300

    Create nozoom.js

---
+++ js/nozoom.js
@@ -0,0 +1,9 @@
+    document.addEventListener("wheel", function(event) {
+        if (event.ctrlKey) {
+            event.preventDefault();
+        }
+    }, { passive: false });
+
+    document.addEventListener("gesturestart", function(event) {
+        event.preventDefault();
+    });
