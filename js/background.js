5d7e237f51cac0574ee269d692c1772c0b4782d6
hamsauno <hamsauno74@gmail.com>
2025-03-23 21:05:34 +0300

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,6 @@
+    document.addEventListener("DOMContentLoaded", function() {
+        document.body.style.backgroundImage = "url('background.jpg')"; 
+        document.body.style.backgroundSize = "cover"; 
+        document.body.style.backgroundPosition = "center"; 
+        document.body.style.backgroundRepeat = "no-repeat";
+    });
