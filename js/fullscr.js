d1354882a47c60744d9a519055b7f06d3045afcd
hamsauno <hamsauno74@gmail.com>
2025-03-23 19:20:25 +0300

    Update fullscr.js

--- js/fullscr.js
+++ js/fullscr.js
@@ -1,7 +1,1 @@
-        // Инициализация Telegram Web App
-        window.Telegram.WebApp.init();
-
-        // Подождите, пока WebView изменит размер, и затем разверните приложение
-        window.Telegram.WebApp.onEvent('viewportChanged', function() {
-            window.Telegram.WebApp.expand();  // Разворачиваем приложение на весь экран
-        });
+Telegram.WebApp.expand();
