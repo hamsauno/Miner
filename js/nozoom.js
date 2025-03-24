863dce333f548f381543a9b69ec96766e9949bed
hamsauno <hamsauno74@gmail.com>
2025-03-24 17:07:50 +0300

    Update nozoom.js

--- js/nozoom.js
+++ js/nozoom.js
@@ -7,3 +7,15 @@
     document.addEventListener("gesturestart", function(event) {
         event.preventDefault();
     });
+
+    document.getElementById("electricityCost").addEventListener("input", function() {
+    // Применение значения и сворачивание клавиатуры
+    this.blur();
+    calculateProfit();
+    });
+    
+    document.getElementById("asicCost").addEventListener("input", function() {
+        // Применение значения и сворачивание клавиатуры
+        this.blur();
+        calculateProfit();
+    });
