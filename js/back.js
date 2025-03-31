970ac05fd6319d6b2c8ee75afccaabe888f7c80c
hamsauno <hamsauno74@gmail.com>
2025-03-31 18:22:57 +0300

    Update back.js

--- js/back.js
+++ js/back.js
@@ -1,7 +1,13 @@
-    Telegram.WebApp.ready(); // Инициализация WebApp
+Telegram.WebApp.ready(); // Инициализация WebApp
+
+// Проверяем, поддерживается ли BackButton
+if (Telegram.WebApp.version && parseFloat(Telegram.WebApp.version) >= 6.1) {
     Telegram.WebApp.BackButton.show(); // Показываем кнопку "Назад"
 
     // Обработчик нажатия кнопки "Назад"
     Telegram.WebApp.BackButton.onClick(function () {
         window.history.back(); // Возвращаемся назад в истории браузера
     });
+} else {
+    console.warn("BackButton не поддерживается в этой версии Telegram Web Apps.");
+}
