d51d4c1bf0d556b4c66057d7bd1c66a4e38d82e8
hamsauno <hamsauno74@gmail.com>
2025-04-01 21:26:23 +0300

    Update menuBtn.js

--- js/menuBtn.js
+++ js/menuBtn.js
@@ -2,17 +2,9 @@ function toggleMenu() {
     const sidebar = document.getElementById("sidebar");
     const overlay = document.getElementById("overlay");
 
-    console.log("Функция toggleMenu вызвана!"); // Проверяем, вызывается ли функция
-    console.log("Состояние до:", sidebar.classList);
-
+    // Переключаем класс 'active' у sidebar и overlay
     sidebar.classList.toggle("active");
     overlay.classList.toggle("active");
 
-    console.log("Состояние после:", sidebar.classList);
-
-    if (sidebar.classList.contains("active")) {
-        document.body.style.overflow = "hidden";
-    } else {
-        document.body.style.overflow = "";
-    }
+    console.log("Функция toggleMenu вызвана!");
 }
