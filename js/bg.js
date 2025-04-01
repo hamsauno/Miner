c160e3a0054e118936efbd63aad4775f107436cc
hamsauno <hamsauno74@gmail.com>
2025-04-01 23:56:40 +0300

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -1,8 +1,8 @@
         // Функция для обработки события изменения ориентации устройства
         function handleOrientation(event) {
             // Получаем данные с устройства
-            const beta = event.beta;  // Наклон по оси X
-            const gamma = event.gamma;  // Наклон по оси Y
+            const beta = event.beta;  // Наклон по оси X (вверх/вниз)
+            const gamma = event.gamma;  // Наклон по оси Y (влево/вправ)
 
             // Ограничиваем значения, чтобы избежать слишком больших изменений
             const x = Math.min(Math.max(gamma, -45), 45); // Положение по оси X
@@ -13,4 +13,4 @@
         }
 
         // Добавляем обработчик события изменения ориентации устройства
-        window.addEventListener('deviceorientation', handleOrientation);
+        window.addEventListener('deviceorientation', handleOrientation, false);
