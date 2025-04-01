ecbf00e0add5e1acf2c93c7a2d045a811a27a9c5
hamsauno <hamsauno74@gmail.com>
2025-04-01 22:36:30 +0300

    Update menuBtn.js

--- js/menuBtn.js
+++ js/menuBtn.js
@@ -6,9 +6,21 @@
             });
         }
 
-        // Функция для скрытия меню
+        // Функция для открытия/закрытия бокового меню
         function toggleMenu() {
-            const sidebar = document.getElementById('sidebar');
-            const overlay = document.getElementById('overlay');
+            const sidebar = document.getElementById("sidebar");
+            const overlay = document.getElementById("overlay");
             sidebar.classList.toggle('active');
             overlay.classList.toggle('active');
+
+            // Проверяем, открыто ли меню
+            if (sidebar.style.left === "0px") {
+                // Если меню открыто, скрываем его
+                sidebar.style.left = "-250px";
+                overlay.style.display = "none";
+            } else {
+                // Если меню закрыто, показываем его
+                sidebar.style.left = "0";
+                overlay.style.display = "block";
+            }
+        }
