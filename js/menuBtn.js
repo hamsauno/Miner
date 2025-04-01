ad50a072d8002abcb6ea74950a13d4ca8df2ea0f
hamsauno <hamsauno74@gmail.com>
2025-04-01 21:44:45 +0300

    Update menuBtn.js

--- js/menuBtn.js
+++ js/menuBtn.js
@@ -1,10 +1,16 @@
-function toggleMenu() {
-    const sidebar = document.getElementById("sidebar");
-    const overlay = document.getElementById("overlay");
+       // Функция для открытия/закрытия бокового меню
+        function toggleMenu() {
+            const sidebar = document.getElementById("sidebar");
+            const overlay = document.getElementById("overlay");
 
-    // Переключаем класс 'active' у sidebar и overlay
-    sidebar.classList.toggle("active");
-    overlay.classList.toggle("active");
-
-    console.log("Функция toggleMenu вызвана!");
-}
+            // Проверяем, открыто ли меню
+            if (sidebar.style.left === "0px") {
+                // Если меню открыто, скрываем его
+                sidebar.style.left = "-250px";
+                overlay.style.display = "none";
+            } else {
+                // Если меню закрыто, показываем его
+                sidebar.style.left = "0";
+                overlay.style.display = "block";
+            }
+        }
