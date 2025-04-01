0c6ffbe804fe825db86402a04a54c3f8e8fdcbdd
hamsauno <hamsauno74@gmail.com>
2025-04-02 01:03:33 +0300

    Delete js/hideback.js

--- js/hideback.js
+++
@@ -1,9 +0,0 @@
-    // Пытаемся скрыть кнопку "Назад" только на мобильных устройствах
-    if (Telegram.WebApp.isMobile) {
-        Telegram.WebApp.BackButton.hide(); // Скрыть кнопку
-    }
-
-    // Если кнопка всё равно отображается, пробуем скрыть её с задержкой
-    setTimeout(function() {
-        Telegram.WebApp.BackButton.hide();
-    }, 1000); // Задержка в 1 секунду
