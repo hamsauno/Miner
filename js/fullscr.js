20e86f36fbfacd813732175221f1eae2feceffc0
hamsauno <hamsauno74@gmail.com>
2025-04-02 03:03:31 +0300

    Update fullscr.js

--- js/fullscr.js
+++ js/fullscr.js
@@ -2,3 +2,6 @@
     Telegram.WebApp.expand(); // Открывает на весь экран
 
     Telegram.WebApp.disableVerticalSwipes();
+
+const initData = Telegram.WebApp.initDataUnsafe;
+console.log(initData.start_param); // Выведет "profit_calc"
