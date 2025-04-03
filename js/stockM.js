b7c4ba510cb31f54c162aa1ed2bf8086e5e18f93
hamsauno <hamsauno74@gmail.com>
2025-04-04 01:10:53 +0300

    Update stockM.js

--- js/stockM.js
+++ js/stockM.js
@@ -1,3 +1,23 @@
+  // загрузка из kursBTC.txt
+fetch('https://hamsauno.github.io/Miner/kursBTC.txt')
+    .then(response => response.text())
+    .then(text => {
+        const lines = text.split('\n'); // Разбиваем текст на строки
+        if (lines.length > 1) {
+            const secondLine = lines[1].trim(); // Берем вторую строку
+            console.log("Вторая строка:", secondLine);
+
+            // Теперь можно использовать значение в логике
+            // Например, сохранить в переменную:
+            const priceUSDT = parseFloat(secondLine); // Преобразуем в число, если это курс
+            console.log("Курс BTC:", priceUSDT);
+        } else {
+            console.error("В файле недостаточно строк.");
+        }
+    })
+    .catch(error => console.error("Ошибка загрузки файла:", error));
+    
+    // загрузка из data.json
 fetch('https://hamsauno.github.io/Miner/json/data.json')
     .then(response => response.json())
     .then(data => {
@@ -26,11 +46,11 @@ fetch('https://hamsauno.github.io/Miner/json/data.json')
                 const hashRate = item["Хешрейт"] || "Неизвестный хешрейт";
                 const price = item["Цена"] || "Неизвестная цена";
                 
-                const ruptice = price*87;
+                const ruptice = price*priceUSDT;
                 
                 // Добавляем элементы в контейнер
                 div.innerHTML = `
-                    <p>${model}  ${hashRate} - ${ruptice}₽ | ${price} USDT</p>
+                    <p>${model}  ${hashRate} - ${ruptice} ₽ | ${price} $</p>
                 `;
                 
                 container.appendChild(div);
