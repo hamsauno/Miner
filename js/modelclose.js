0e749a904c1b8d85207cfba6fee05be334ca0b4a
hamsauno <hamsauno74@gmail.com>
2025-04-05 14:56:51 +0000

    Deploying to gh-pages from @ hamsauno/Miner@637aa235b1768f0bb1ecb5215e902dcf02ae34a2 🚀

--- js/modelclose.js
+++ js/modelclose.js
@@ -1,20 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('product-modal');
     const closeBtn = document.querySelector('.close-button');
-    let isModalOpen = false; // Флаг, чтобы отслеживать, открыт ли модал
 
-    // Закрытие при клике на кнопку "×"
     closeBtn.addEventListener('click', () => {
-        if (isModalOpen) {
-            modal.classList.remove('show');
-            isModalOpen = false;
-        }
+        modal.style.display = 'none';
     });
 
-    // Закрытие при клике вне модального окна
     window.addEventListener('click', (e) => {
-        if (e.target === modal && isModalOpen) {
-            modal.classList.remove('show');
-            isModalOpen = false;
+        if (e.target === modal) {
+            modal.style.display = 'none';
         }
     });
+});
