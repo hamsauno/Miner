9cb9ba04baa587f5386e978df1d56cf86609f69a
hamsauno <hamsauno74@gmail.com>
2025-04-19 19:31:19 +0000

    Deploying to gh-pages from @ hamsauno/Miner@8f692bcf4ec9b71d765b6023cbd703c33bd6a7ec 🚀

--- js/modelsBTC_LTC.js
+++ js/modelsBTC_LTC.js
@@ -193,3 +193,15 @@ let jsonData = [];
    document.getElementById("roi").innerText = roi.toFixed(2);
    document.getElementById("payback").innerText = payback.toFixed(0);
  }
+
+function updateRubTable() {
+    const usdIds = ['income', 'incomeMonth', 'incomeYear', 'profit', 'profitMonth', 'profitYear'];
+    
+    usdIds.forEach(id => {
+        const usdVal = parseFloat(document.getElementById(id).innerText) || 0;
+        const rubId = id + '_rub';
+        const rubVal = (usdVal * usdtPrice).toFixed(2);
+        document.getElementById(rubId).innerText = rubVal;
+    });
+}
+
